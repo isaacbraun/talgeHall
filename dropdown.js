ef09127167630cdc73d2d89800a1a879620edf26
isaacbraun <isaacbraun2000@gmail.com>
2020-07-29 13:21:45 -0400

    Added dropdown.js and it's functionality

---
+++ dropdown.js
@@ -0,0 +1,10 @@
+function dropdown() {
+    let menu = document.getElementsByClassName('dropdown-menu');
+
+    if (menu.style.display = 'none') {
+        menu.style.display = 'block';
+    }
+    else {
+        menu.style.display = 'none';
+    }
+}
