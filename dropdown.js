d5782776a89fcf9b749d23ddc638a8353b42a696
isaacbraun <isaacbraun2000@gmail.com>
2020-07-29 15:14:50 -0400

    Switched to changing classes

--- dropdown.js
+++ dropdown.js
@@ -1,14 +1,15 @@
 function dropdown() {
     let menu = document.getElementsByClassName('dropdown-menu')[0];
 
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block';
-        menu.style.height = 'auto';
-    }
-    else {
-        menu.style.display = 'none';
-        menu.style.height = '0';
-    }
+    menu.classList.toggle('transition');
+
+    // if (menu.style.display == 'none') {
+    //     menu.style.display = 'block';
+    //     menu.style.height = 'auto';
+    // }
+    // else {
+    //     menu.style.display = 'none';
+    // }
 };
 
 
