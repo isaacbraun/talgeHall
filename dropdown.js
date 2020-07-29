854106663082add92a0639e390c1779321b97e78
isaacbraun <isaacbraun2000@gmail.com>
2020-07-29 13:28:52 -0400

    Fixed selector query

--- dropdown.js
+++ dropdown.js
@@ -1,10 +1,10 @@
 function dropdown() {
-    let menu = document.getElementsByClassName('dropdown-menu');
+    let menu = document.getElementsByClassName('dropdown-menu')[0];
 
-    if (menu.style.display = 'none') {
+    if (menu.style.display == 'none') {
         menu.style.display = 'block';
     }
     else {
         menu.style.display = 'none';
     }
-}
+};
