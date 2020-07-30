571139d9bc67cfda3775a90542720fa6f0fcea4e
isaacbraun <isaacbraun2000@gmail.com>
2020-07-30 12:49:30 -0400

    Finished Dropdown CSS

--- dropdown.js
+++ dropdown.js
@@ -1,9 +1,7 @@
 function dropdown() {
     let menu = document.getElementsByClassName('dropdown-menu-container')[0];
-    let square = document.getElementsByClassName('square')[0];
 
     menu.classList.toggle('transition');
-    square.classList.toggle('rotate');
 };
 
 
